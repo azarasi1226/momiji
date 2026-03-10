e6a9c2ac5c3219dbf500963b2d3b47b28287a163
andou_kazuki <azarasikazuki@gmail.com>
2026-03-10 12:50:23 +0900

    save

--- backend/src/main/kotlin/jp/momiji/feature/user/changeemail/confirm/ConfirmEmailChangeController.kt
+++ backend/src/main/kotlin/jp/momiji/feature/user/changeemail/confirm/ConfirmEmailChangeController.kt
@@ -6,18 +6,20 @@ import jp.momiji.feature.throwIfError
 import jp.momiji.feature.user.UserIdResolver
 import org.axonframework.messaging.commandhandling.gateway.CommandGateway
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+
+data class ConfirmEmailChangeBody(val token: String)
 
 @RestController
 class ConfirmEmailChangeController(
   private val commandGateway: CommandGateway,
   private val userIdResolver: UserIdResolver,
 ) {
-  @GetMapping("/users/me/email/change-confirm")
+  @PostMapping("/users/me/email/change-confirm")
   fun confirmEmailChange(
-    @RequestParam token: String,
+    @RequestBody body: ConfirmEmailChangeBody,
     authentication: JwtAuthenticationToken,
   ) {
     val userId = userIdResolver.resolve(authentication)
@@ -26,7 +28,7 @@ class ConfirmEmailChangeController(
     commandGateway.confirmEmailChange(
       ConfirmEmailChangeCommand(
         userId = userId,
-        token = token,
+        token = body.token,
       )
     ).throwIfError()
   }
