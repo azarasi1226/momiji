cb98c271a26f36f05388042bcdee5668c52fd905
andou_kazuki <azarasikazuki@gmail.com>
2026-03-11 10:55:58 +0900

    さヴぇ

--- backend/src/main/kotlin/jp/momiji/feature/ExceptionAdvice.kt
+++ backend/src/main/kotlin/jp/momiji/feature/ExceptionAdvice.kt
@@ -13,4 +13,11 @@ class ExceptionAdvice {
       .status(HttpStatus.BAD_REQUEST)
       .body(mapOf("error" to (e.error.message)))
   }
+
+  @ExceptionHandler(Exception::class)
+  fun handleException(e: Exception): ResponseEntity<Map<String, String>> {
+    return ResponseEntity
+      .status(HttpStatus.INTERNAL_SERVER_ERROR)
+      .body(mapOf("error" to ("原因不明のエラー")))
+  }
 }
