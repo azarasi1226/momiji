9df758963652fd192f60f0cd66d8fa5258898d34
andou_kazuki <azarasikazuki@gmail.com>
2026-06-03 01:48:01 +0900

    リファクタリング

---
+++ backend/src/main/kotlin/jp/momiji/feature/idp/IdpUserInfoFetcher.kt
@@ -0,0 +1,23 @@
+package jp.momiji.feature.idp
+
+import com.github.michaelbull.result.getOrElse
+import jp.momiji.domain.idp.IdentityProvider
+import jp.momiji.domain.user.Email
+
+data class OidcUserInfo(
+    val issuer: String,
+    val subject: String,
+    val email: Email,
+    val emailVerified: Boolean,
+    val identityProvider: IdentityProvider,
+)
+
+interface IdpUserInfoFetcher {
+    fun handle(
+        subject: String,
+        issuer: String,
+    ): OidcUserInfo
+}
+
+// IDPから送られてくる email が万が一正しくない場合でもユーザーはどうすることもできないため IllegalStateExceptionとする。
+internal fun resolveEmail(rawEmail: String): Email = Email.create(rawEmail).getOrElse { throw IllegalStateException(it.message) }
