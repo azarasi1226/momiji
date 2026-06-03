944671675ec9496a6f240ca8246dd157bfa79bf8
andou_kazuki <azarasikazuki@gmail.com>
2026-06-03 09:19:22 +0900

    リファクタリング

--- backend/src/main/kotlin/jp/momiji/feature/user/changeemail/confirm/EmailToIdpSyncer.kt
+++ backend/src/main/kotlin/jp/momiji/feature/user/changeemail/confirm/EmailToIdpSyncer.kt
@@ -18,14 +18,15 @@ class EmailToIdpSyncer(
 ) {
     @EventHandler
     fun on(event: EmailChangeConfirmedEvent) {
-        // IdentityProviderが "LOCAL" のものだけを対象とする。　これには二つの意味がある。
-        // 1. 例えば "Google" なものを変更したとしても、IDPにログインするたびにIDP内のemail属性が書き換えられるから意味がない。
-        // 2. "LOCAL" はログイン時にemailを使用するので、絶対同期させる必要があるが、 "Google" などの場合はログインに使用しないし、参照もしないため。
+
         val oidcSubjects =
             dsl
                 .select(LOOKUP_EXTERNAL_IDENTITIES.OIDC_SUBJECT)
                 .from(LOOKUP_EXTERNAL_IDENTITIES)
                 .where(LOOKUP_EXTERNAL_IDENTITIES.USER_ID.eq(event.userId))
+                // IdentityProviderが "LOCAL" のものだけを対象とする。　これには二つの意味がある。
+                // 1. 例えば "Google" なものを変更したとしても、ソーシャルIDPにログインするたびにIDP内のemail属性が書き換えられるから結局意味がない。
+                // 2. "LOCAL" はログイン時にemailを使用するので、絶対同期させる必要があるが、 "Google" などの場合はログインに使用しないし、参照もしないため。ただの保続情報として属性を持っているだけの模様
                 .and(LOOKUP_EXTERNAL_IDENTITIES.IDENTITY_PROVIDER.eq(IdentityProvider.LOCAL.name))
                 .fetch(LOOKUP_EXTERNAL_IDENTITIES.OIDC_SUBJECT)
                 .filterNotNull()
