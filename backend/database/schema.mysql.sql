e6ad80506d42ca2069bc9bda7ec6f4535a3d3835
andou_kazuki <azarasikazuki@gmail.com>
2026-06-06 11:21:35 +0900

    DB マイグレーション

--- backend/database/schema.mysql.sql
+++ backend/database/schema.mysql.sql
@@ -31,6 +31,20 @@ CREATE TABLE brands (
   id varchar(255) NOT NULL,
   name varchar(255) NOT NULL,
   description text NOT NULL,
+  status varchar(255) NOT NULL,
+  created_at datetime(6) NOT NULL,
+  updated_at datetime(6) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE products (
+  id varchar(255) NOT NULL,
+  brand_id varchar(255) NOT NULL,
+  name varchar(255) NOT NULL,
+  description text NOT NULL,
+  image_url varchar(255),
+  price int NOT NULL,
+  status varchar(255) NOT NULL,
   created_at datetime(6) NOT NULL,
   updated_at datetime(6) NOT NULL,
   PRIMARY KEY (id)
